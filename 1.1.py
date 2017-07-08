41da9f60e00de337ed6136fe030632e559d1455f
8MarkOut <wudixjh@sina.com>
2017-07-08 11:43:27 +0800

    script

---
+++ 1.1.py
@@ -0,0 +1,75 @@
+# 引入库
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import model_selection, preprocessing
+import xgboost as xgb
+color = sns.color_palette()
+
+import datetime
+from scipy.stats import norm
+import scipy as sp
+import statsmodels.api as sm
+
+%matplotlib inline
+import xlrd
+
+# 导入数据
+
+train = pd.read_csv("./input/train.csv", parse_dates=['timestamp'])
+test = pd.read_csv("./input/test.csv", parse_dates=['timestamp'])
+macro = pd.read_csv("./input/macro.csv", parse_dates=['timestamp'])
+id_test = test.id
+
+fx = pd.read_excel('./input/BAD_ADDRESS_FIX.xlsx').drop_duplicates('id').set_index('id')
+train.update(fx)
+test.update(fx)
+
+train = pd.merge(train, macro, on='timestamp')
+test = pd.merge(test, macro, on='timestamp')
+
+y_train = train["price_doc"]
+x_train = train.drop(["id", "timestamp", "price_doc"], axis=1)
+x_test = test.drop(["id", "timestamp"], axis=1)
+
+#can't merge train with test because the kernel run for very long time
+
+for c in x_train.columns:
+    if x_train[c].dtype == 'object':
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(list(x_train[c].values)) 
+        x_train[c] = lbl.transform(list(x_train[c].values))
+        #x_train.drop(c,axis=1,inplace=True)
+        
+for c in x_test.columns:
+    if x_test[c].dtype == 'object':
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(list(x_test[c].values)) 
+        x_test[c] = lbl.transform(list(x_test[c].values))
+        #x_test.drop(c,axis=1,inplace=True)     
+
+xgb_params = {
+    'eta': 0.05,
+    'max_depth': 5,
+    'subsample': 0.7,
+    'colsample_bytree': 0.7,
+    'objective': 'reg:linear',
+    'eval_metric': 'rmse',
+    'silent': 1
+}
+
+dtrain = xgb.DMatrix(x_train, y_train)
+dtest = xgb.DMatrix(x_test)
+
+cv_output = xgb.cv(xgb_params, dtrain, num_boost_round=1000, early_stopping_rounds=20,
+    verbose_eval=50, show_stdv=False)
+cv_output[['train-rmse-mean', 'test-rmse-mean']].plot()
+
+num_boost_rounds = len(cv_output)
+model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round= num_boost_rounds)
+
+y_predict = model.predict(dtest)
+output = pd.DataFrame({'id': id_test, 'price_doc': y_predict})
+output.head()
